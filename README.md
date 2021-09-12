be1f590b5313c95ccafc34b99d5095bbb0a24973
ylabo0717 <Yosuke.Horie@sony.com>
2021-09-13 07:46:23 +0900

    Add new files.

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# codeco
+# CoDeCo
+
+CoDeCo is a code decorator extenstion for Visual Studio Code.  
+This extension provides the following features.
+
+* Display EOL (new line) characters.
+* Display Full-width Space.
+
+## Example
+
+### Display EOL (End Of Line) characters.
+
+#### LF
+
+![](screenshot/lf.png)
+
+#### CR+LF
+
+![](screenshot/crlf.png)
+
+### Display Full-width Space.
+
+![](screenshot/full-width_space.png)
+
+## Thanks
+
+### decorator-sample
+
+https://github.com/microsoft/vscode-extension-samples/tree/main/decorator-sample
+
+### code-eol
+
+https://github.com/sohamkamani/code-eol
+
+
+## License
+
+Distributed under the MIT License. See [LICENSE](./LICENSE) for more information.
---
+++ src/extension.ts
@@ -0,0 +1,96 @@
+import * as vscode from 'vscode';
+
+// this method is called when vs code is activated
+export function activate(context: vscode.ExtensionContext) {
+	let timeout: NodeJS.Timer | undefined = undefined;
+
+	// create a decorator type that we use to decorate end of line
+	const eolDecoration = vscode.window.createTextEditorDecorationType({});
+	const lfColor = { id: 'codeco.lfColor' };
+	const crlfColor = { id: 'codeco.crlfColor' };
+	const crColor = { id: 'codeco.crColor' };
+
+	// create a decorator type that we use to decorate full-width space
+	const fullWidthSpaceDecorationType = vscode.window.createTextEditorDecorationType({
+		cursor: 'crosshair',
+		backgroundColor: { id: 'codeco.fullWidthSpaceBackground' }
+	});
+
+	let activeEditor = vscode.window.activeTextEditor;
+
+	function updateDecorations() {
+		if (!activeEditor) {
+			return;
+		}
+		const text = activeEditor.document.getText();
+		const regEx = /(\u3000)|(\r(?!\n))|(\r?\n)/g;
+		const fullWidthSpace: vscode.DecorationOptions[] = [];
+		const eol: vscode.DecorationOptions[] = [];
+		let match;
+		while ((match = regEx.exec(text))) {
+			if (/\u3000/g.test(match[0])) {
+				const startPos = activeEditor.document.positionAt(match.index);
+				const endPos = activeEditor.document.positionAt(match.index + match[0].length);
+				const decoration = { range: new vscode.Range(startPos, endPos) };
+				fullWidthSpace.push(decoration);
+			} else {
+				const decorationText = getEolDecorationInfo(match[0]);
+				const decTxt = decorationText[0];
+				const decColor = decorationText[1];
+				const startPos = activeEditor.document.positionAt(match.index);
+				const endPos = activeEditor.document.positionAt(match.index);
+				const decoration = {
+					range: new vscode.Range(startPos, endPos),
+					renderOptions: {
+						after: {
+							contentText: decTxt,
+							color: decColor
+						}
+					}
+				};
+				eol.push(decoration);
+			}
+		}
+		activeEditor.setDecorations(fullWidthSpaceDecorationType, fullWidthSpace);
+		activeEditor.setDecorations(eolDecoration, eol);
+	}
+
+	function triggerUpdateDecorations() {
+		if (timeout) {
+			clearTimeout(timeout);
+			timeout = undefined;
+		}
+		timeout = setTimeout(updateDecorations, 500);
+	}
+
+	if (activeEditor) {
+		triggerUpdateDecorations();
+	}
+
+	vscode.window.onDidChangeActiveTextEditor(editor => {
+		activeEditor = editor;
+		if (editor) {
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
+
+	vscode.workspace.onDidChangeTextDocument(event => {
+		if (activeEditor && event.document === activeEditor.document) {
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
+
+	var getEolDecorationInfo = function(match: string) : [string, {id: string}] {
+		switch (match) {
+			case '\n':       // LF
+				return ['↓', lfColor];
+			case '\r\n':     // CR+LF
+				return ['↵', crlfColor];
+			case '\r':       // CR
+				return ['←', crColor];
+			default:
+				return ['', {id: ''}];
+		}
+	};
+}
+
